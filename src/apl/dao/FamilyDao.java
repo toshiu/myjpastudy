0f604b5ce1bc7e9224b8584bbd27d99902ed2a41
ZhiYuDu <duzhiyu@vip.qq.com>
2018-09-18 20:20:25 +0800

    201809182020

--- src/apl/dao/FamilyDao.java
+++ src/apl/dao/FamilyDao.java
@@ -1,8 +1,15 @@
 package apl.dao;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 import apl.entity.Family;
 import apl.entity.FamilyPK;
@@ -37,4 +44,89 @@ public class FamilyDao {
 		}
 		return f;
 	}
+	
+	public Family findByMan(String man) {
+		Family f = null;
+		
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("select f from Family f where f.id.man = :man");
+			q.setParameter("man", man);
+			f = (Family) q.getResultList().get(0);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+		return f;
+	}
+	
+	public List<Family> findDescList() {
+		List<Family> fList = null;
+		
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("select f from Family f order by f.address desc NULLS FIRST");
+			fList = (List<Family>) q.getResultList();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+		return fList;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public List<Map<String, Object>> findManAndWomanList() {
+		List<Map<String, Object>> resList = new ArrayList<Map<String, Object>>();
+		
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("select f.id.man, f.id.woman from Family f");
+			List jpqlRes= q.getResultList();
+			if (jpqlRes!=null){
+				Iterator iterator = jpqlRes.iterator();
+				while( iterator.hasNext() ){
+					Object[] row = (Object[]) iterator.next();
+					Map<String, Object> resMap = new HashMap<String, Object>();
+					resMap.put("man", row[0].toString());
+					resMap.put("woman", row[1].toString());
+					resList.add(resMap);
+				}
+			}
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+		return resList;
+	}
+	
+	@SuppressWarnings("rawtypes")
+	public List<Map<String, Object>> findCityManList() {
+		List<Map<String, Object>> resList = new ArrayList<Map<String, Object>>();
+		
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("select f.id.man, f.id.woman, c.cityName from Family f left join City c on f.id.man = c.cityMan");
+			List jpqlRes= q.getResultList();
+			if (jpqlRes!=null){
+				Iterator iterator = jpqlRes.iterator();
+				while( iterator.hasNext() ){
+					Object[] row = (Object[]) iterator.next();
+					Map<String, Object> resMap = new HashMap<String, Object>();
+					resMap.put("man", row[0].toString());
+					resMap.put("woman", row[1].toString());
+					resMap.put("cityName", row[2] == null ? null : row[2].toString());
+					resList.add(resMap);
+				}
+			}
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			em.getTransaction().rollback();
+		}
+		return resList;
+	}
+	
 }
--- src/apl/startup/FamilyStartup.java
+++ src/apl/startup/FamilyStartup.java
@@ -1,6 +1,8 @@
 package apl.startup;
 
 import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
 
 import apl.dao.FamilyDao;
 import apl.entity.Family;
@@ -11,19 +13,36 @@ public class FamilyStartup {
 	public static void main(String[] args) throws ParseException {
 		FamilyDao familyDao = new FamilyDao();
 		
-		FamilyPK fpk1 = new FamilyPK();
-		fpk1.setMan("Duzhiyu");
-		fpk1.setWoman("Wangkun");
-		
-		Family f1 = new Family();
-		f1.setId(fpk1);
-		f1.setAddress("Shanghai");
-		
+//		FamilyPK fpk1 = new FamilyPK();
+//		fpk1.setMan("Wangjianping");
+//		fpk1.setWoman("Zhanghuafang");
+//		
+//		Family f1 = new Family();
+//		f1.setId(fpk1);
+//		f1.setAddress(null);
+//		
 //		familyDao.insert(f1);
+//		
+//		Family x = familyDao.findByPk(fpk1);
+//		System.out.println(x.getId().getMan());
+//		System.out.println(x.getAddress());
 		
-		Family x = familyDao.findByPk(fpk1);
-		System.out.println(x.getId().getMan());
-		System.out.println(x.getAddress());
+//		Family x = familyDao.findByMan("Duzhiyu");
+//		System.out.println(x.getAddress());
+		
+//		java.util.List<Family> x = familyDao.findDescList();
+//		System.out.println(x.get(0).getAddress());
+//		System.out.println(x.get(x.size() - 1).getAddress());
+		
+//		List<Map<String, Object>> x = familyDao.findManAndWomanList();
+//		for (Map<String, Object> m : x) {
+//			System.out.println(m);
+//		}
+		
+		List<Map<String, Object>> x = familyDao.findCityManList();
+		for (Map<String, Object> m : x) {
+			System.out.println(m);
+		}
 	}
 
 }
